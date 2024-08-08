f652da6b8ae99d812ad59601a3b9cc6e8a2fb171
Eric Grebe <jiyuu@rockbiter.de>
2024-08-08 13:58:44 +0200

    add files

---
+++ app/src/main/java/com/example/dessertclicker/ui/DessertUiState.kt
@@ -0,0 +1,9 @@
+package com.example.dessertclicker.ui
+
+data class DessertUiState (
+    val desertsSold: Int = 0,
+    val revenue: Int = 0,
+    val currentDessertIndex: Int = 0,
+    val currentDessertPrice: Int = 0,
+    val currentDessertImageId: Int = 0,
+)
